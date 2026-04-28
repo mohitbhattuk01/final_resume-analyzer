5559196cfdb5bd13ae4d6743aa9183f5cbcaca42
mohitbhattuk01 <mohitbhatt2030@gmail.com>
2026-04-28 12:19:58 +0530

    Update: app.py latest changes

--- resume_portal/app.py
+++ resume_portal/app.py
@@ -488,50 +488,6 @@
 # #     print("="*60)
     
 # #     app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for, make_response, send_from_directory
 import os
 import json
